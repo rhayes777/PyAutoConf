664345f3a6cb7fc1ba51890c5bdf27c31a3a6262
Richard <richardhayes777@gmail.com>
2020-02-14 12:01:13 +0000

    extracted fixture

--- test_autoconf/test_class_path.py
+++ test_autoconf/test_class_path.py
@@ -1,18 +1,26 @@
 import autoconf as ac
 from test_autoconf.mock import GeometryProfile
+import pytest
 
 
-def test_path_for_class():
+@pytest.fixture(
+    name="geometry_profile_path"
+)
+def make_geometry_profile_path():
+    return "test_autoconf.mock.GeometryProfile"
+
+
+def test_path_for_class(geometry_profile_path):
     assert ac.path_for_class(
         GeometryProfile
-    ) == "test_autoconf.mock.GeometryProfile"
+    ) == geometry_profile_path
 
 
-def test_config_for_path():
+def test_config_for_path(geometry_profile_path):
     assert ac.JSONPriorConfig(
         {
             "test_autoconf.mock.GeometryProfile": "test"
         }
     )(
-        "test_autoconf.mock.GeometryProfile"
+        geometry_profile_path
     ) == "test"
