7939ac59550fa09f5f959cb499ec184ef76ff71b
Richard <richardhayes777@gmail.com>
2020-03-02 14:33:57 +0000

    removed old json config

--- test_autoconf/json_prior/test_conversion.py
+++ test_autoconf/json_prior/test_conversion.py
@@ -11,11 +11,6 @@ def make_prior_directory():
     return str(Path(__file__).parent.parent / "test_files" / "config" / "priors")
 
 
-@pytest.fixture(name="prior_filename")
-def make_prior_filename(prior_directory):
-    return f"{prior_directory}.json"
-
-
 @pytest.fixture(name="converter")
 def make_converter(prior_directory):
     return c.Converter(prior_directory)
@@ -24,11 +19,6 @@ def make_converter(prior_directory):
 @pytest.fixture(name="prior_json")
 def make_prior_json(converter):
     return converter.dict
-
-
-def test_convert(prior_directory, prior_filename):
-    c.convert(prior_directory)
-    assert os.path.exists(prior_filename)
 
 
 def test_modules(converter):
