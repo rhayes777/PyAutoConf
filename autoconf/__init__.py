d285baed987dc2c2bfcfdd237e43b16dcca138bb
Richard <richardhayes777@gmail.com>
2020-10-15 16:56:14 +0100

    relative imports make edenise happy

--- autoconf/__init__.py
+++ autoconf/__init__.py
@@ -1,9 +1,9 @@
-from autoconf import exc
-from autoconf.conf import Config
-from autoconf.conf import instance
-from autoconf.json_prior.config import default_prior
-from autoconf.json_prior.config import make_config_for_class
-from autoconf.json_prior.config import path_for_class
-from autoconf.json_prior.config import JSONPriorConfig
+from . import exc
+from .conf import Config
+from .conf import instance
+from .json_prior.config import default_prior
+from .json_prior.config import make_config_for_class
+from .json_prior.config import path_for_class
+from .json_prior.config import JSONPriorConfig
 
 __version__ = '0.6.2'
