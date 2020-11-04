a5771c34ae8dc1c1639010ed22e588525715d4f2
Richard <richardhayes777@gmail.com>
2020-11-04 11:43:35 +0000

    docs and unused function

--- autoconf/conf.py
+++ autoconf/conf.py
@@ -14,15 +14,31 @@ def get_matplotlib_backend():
 
 class Config(ConfigWrapper):
     def __init__(self, *config_paths, output_path="output"):
+        """
+        Singleton to manage configuration.
+
+        Configuration is loaded using the __getitem__ syntax where the key entered
+        can refer to a directory, file, section or item.
+
+        Configuration is first attempted to be loaded from the directory indicated by the first
+        config_path. If no configuration is found the second directory is searched and so on.
+        This allows a default configuration to be defined with additional configuration overriding
+        it.
+
+        Parameters
+        ----------
+        config_paths
+            Indicate directories where configuration is defined, in the order of priority with
+            configuration in the first config_path overriding configuration in later config
+            paths
+        output_path
+            The path where data should be saved.
+        """
         super().__init__(list(map(
             RecursiveConfig,
             config_paths
         )))
         self.output_path = output_path
-
-    @property
-    def path(self):
-        return self.configs[0].path
 
     @property
     def prior_config(self):
