133613fa5f58caf85a23f8502bf63903f5e43e4a
James Nightingale <james.w.nightingale@durham.ac.uk>
2020-11-29 16:20:42 +0000

    Incremented version number

--- autoconf/__init__.py
+++ autoconf/__init__.py
@@ -6,4 +6,4 @@ from .json_prior.config import make_config_for_class
 from .json_prior.config import path_for_class
 from .json_prior.config import JSONPriorConfig
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
