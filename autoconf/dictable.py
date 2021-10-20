0cc0ab20f59277552d7770d07d23e6a557feb854
Richard <richardhayes777@gmail.com>
2021-10-20 14:42:55 +0100

    added class_path and dictable utils

---
+++ autoconf/dictable.py
@@ -0,0 +1,80 @@
+import inspect
+
+from autofit.tools.util import get_class_path, get_class
+
+
+def as_dict(
+        obj
+):
+    if obj.__class__.__module__ == 'builtins':
+        return obj
+    argument_dict = {
+        arg: getattr(
+            obj, arg
+        )
+        for arg
+        in inspect.getfullargspec(
+            obj.__init__
+        ).args[1:]
+    }
+    return {
+        "type": get_class_path(
+            obj.__class__
+        ),
+        **{
+            key: as_dict(value)
+            for key, value
+            in argument_dict.items()
+        }
+    }
+
+
+class Dictable:
+    def dict(self) -> dict:
+        """
+        A dictionary representation of the instance comprising a type
+        field which contains the entire class path by which the type
+        can be imported and constructor arguments.
+        """
+        return as_dict(self)
+
+    @staticmethod
+    def from_dict(
+            profile_dict
+    ):
+        """
+        Instantiate a GeometryProfile from its dictionary representation.
+
+        Parameters
+        ----------
+        profile_dict
+            A dictionary representation of the instance comprising a type
+            field which contains the entire class path by which the type
+            can be imported and constructor arguments.
+
+        Returns
+        -------
+        An instance of the geometry profile specified by the type field in
+        the profile_dict
+        """
+        if not isinstance(
+                profile_dict,
+                dict
+        ):
+            return profile_dict
+
+        cls = get_class(
+            profile_dict.pop(
+                "type"
+            )
+        )
+        # noinspection PyArgumentList
+        return cls(
+            **{
+                Dictable.from_dict(
+                    name
+                ): value
+                for name, value
+                in profile_dict.items()
+            }
+        )
