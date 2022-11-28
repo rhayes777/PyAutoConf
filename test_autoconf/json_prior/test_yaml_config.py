97ce451ba7fdb065767fecfee0a4aa8138ba1104
Richard <richardhayes777@gmail.com>
2022-11-28 11:09:13 +0000

    also support loading yaml

--- test_autoconf/json_prior/test_yaml_config.py
+++ test_autoconf/json_prior/test_yaml_config.py
@@ -1,19 +1,6 @@
-class JSONClass:
-    def __init__(self, variable: float):
-        self.variable = variable
-
-
 class YAMLClass:
     def __init__(self, variable: float):
         self.variable = variable
-
-
-def test_load_json_config(config):
-    assert config.prior_config.for_class_and_suffix_path(JSONClass, ["variable"]) == {
-        "lower_limit": 0.0,
-        "type": "Uniform",
-        "upper_limit": 3.0,
-    }
 
 
 def test_load_yaml_config(config):
