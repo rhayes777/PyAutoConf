63dbe36c228b69cf357f19bc8d6cac4397009595
Richard <richardhayes777@gmail.com>
2022-11-28 12:08:19 +0000

    test illustrating subdirectory prior config works correctly

--- test_autoconf/json_prior/test_yaml_config.py
+++ test_autoconf/json_prior/test_yaml_config.py
@@ -1,3 +1,6 @@
+from .source_code.subdirectory.subconfig import SubClass
+
+
 class YAMLClass:
     def __init__(self, variable: float):
         self.variable = variable
@@ -14,3 +17,11 @@ def test_load_yaml_config(config):
 def test_embedded_path(config):
     path_value_map = config.prior_config.prior_configs[0].path_value_map
     assert "subdirectory.subconfig.SubClass.variable.type" in path_value_map
+
+
+def test_subdirectory(config):
+    assert config.prior_config.for_class_and_suffix_path(SubClass, ["variable"]) == {
+        "lower_limit": 0.0,
+        "type": "Uniform",
+        "upper_limit": 3.0,
+    }
