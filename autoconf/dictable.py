385e8cbe9645f55402d8c41d74eaac16418f0086
Richard <richard@rghsoftware.co.uk>
2023-09-11 10:12:00 +0100

    aliging...

--- autoconf/dictable.py
+++ autoconf/dictable.py
@@ -78,13 +78,13 @@ def instance_as_dict(obj):
     }
 
 
-def from_dict(cls_dict):
+def from_dict(dictionary):
     """
     Instantiate an instance of a class from its dictionary representation.
 
     Parameters
     ----------
-    cls_dict
+    dictionary
         A dictionary representation of the instance comprising a type
         field which contains the entire class path by which the type
         can be imported and constructor arguments.
@@ -94,31 +94,33 @@ def from_dict(cls_dict):
     An instance of the geometry profile specified by the type field in
     the cls_dict
     """
-    if isinstance(cls_dict, list):
-        return list(map(from_dict, cls_dict))
-    if not isinstance(cls_dict, dict):
-        return cls_dict
-    type_ = cls_dict["type"]
+    if isinstance(dictionary, (int, float, str, bool, type(None))):
+        return dictionary
+
+    if isinstance(dictionary, list):
+        return list(map(from_dict, dictionary))
+
+    type_ = dictionary["type"]
 
     if type_ == "ndarray":
-        return nd_array_from_dict(cls_dict)
+        return nd_array_from_dict(dictionary)
 
     if type_ == "list":
-        return list(map(from_dict, cls_dict["values"]))
+        return list(map(from_dict, dictionary["values"]))
     if type_ == "dict":
-        return {key: from_dict(value) for key, value in cls_dict.items()}
+        return {key: from_dict(value) for key, value in dictionary.items()}
 
     if type_ == "type":
-        return get_class(cls_dict["class_path"])
+        return get_class(dictionary["class_path"])
 
-    cls = get_class(cls_dict["class_path"])
+    cls = get_class(dictionary["class_path"])
 
     if cls is np.ndarray:
-        return nd_array_from_dict(cls_dict)
+        return nd_array_from_dict(dictionary)
 
     # noinspection PyArgumentList
     return cls(
-        **{name: from_dict(value) for name, value in cls_dict["arguments"].items()}
+        **{name: from_dict(value) for name, value in dictionary["arguments"].items()}
     )
 
 
