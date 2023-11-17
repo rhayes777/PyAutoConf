5fd24899db34ef076611c9c7400b3efcb3974b0a
Richard <richard@rghsoftware.co.uk>
2023-11-17 14:32:23 +0000

    test no output

--- test_autoconf/test_output_config.py
+++ test_autoconf/test_output_config.py
@@ -26,3 +26,8 @@ def add_config(files_directory):
 def test_output(output_class):
     output_class.output_function("should_output")
     assert output_class.output_names == ["should_output"]
+
+
+def test_no_output(output_class):
+    output_class.output_function("should_not_output")
+    assert output_class.output_names == []
